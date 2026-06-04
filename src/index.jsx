bc7cd2bcee6a16472c8affa7e179cdd6e6afa7db
Sourabhmane2303 <141675326+Sourabhmane2303@users.noreply.github.com>
2026-06-04 18:32:29 +0530

    Fix import and rendering method in index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import ReactDom from 'react-dom'
+import ReactDOM from 'react-dom/client'
 import App from './App'
 
-ReactDom.createRoot(Document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <App/>
     </React.StrictMode>
 )
-
