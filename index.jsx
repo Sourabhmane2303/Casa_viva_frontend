b8606bcd9e26aa6768c3ef431be6da309cd0b81a
Sourabhmane2303 <141675326+Sourabhmane2303@users.noreply.github.com>
2026-06-04 14:45:25 +0530

    Add files via upload

---
+++ index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+import App from './App'
+
+ReactDom.createRoot(Document.getElementById('root')).render(
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>
+)
+
